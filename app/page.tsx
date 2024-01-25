852e4985d282f6972a89e1590c2ef7d99212e32d
Magib <112185392+mageeeb@users.noreply.github.com>
2024-01-25 16:48:08 +0100

    db

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,31 @@
 import Image from 'next/image'
 import Link from "next/link";
 import Button from "@/app/ui/Button";
+import {fetch} from "undici-types";
 
-export default function Home() {
+
+const getData = async () =>{
+  const res = await fetch("http://localhost:4000/articles\n");
+  const data = await res.json();
+  return data;
+}
+export default async function Home() {
+  const articles = await getData();
   return (
     <main className= "">
       <h2>Bienvenue sur NextJS</h2>
         <Link href={"/connexion"}>Connectez-vous</Link>
         <Button />
+
+      <div className="container">{
+        articles.map((articles)=>(
+            <div key={articles.id} className="carte">
+              <h3>{articles.id}</h3>
+              <p>{articles.titre}</p></div>
+        ))
+      }
+
+      </div>
     </main>
   )
 }
